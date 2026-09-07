b751ea76bcaedd4efd4c97e336c6cbbb307b8c58
Orapeleng-madibela <122809212+Orapeleng-madibela@users.noreply.github.com>
2026-09-07 13:22:33 +0200

    Update test_classifier.py

--- data/test_classifier.py
+++ data/test_classifier.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND/intropylab-classifying-images/test_classifier.py
 #                                                                             
-# PROGRAMMER: Jennifer S.                                                    
-# DATE CREATED: 01/30/2018                                  
+# PROGRAMMER: ORAPELENG TIMOTHY MADIBELA                                                   
+# DATE CREATED: 06 SEPTEMBER 2026                              
 # REVISED DATE:             <=(Date Revised - if any)                         
 # PURPOSE: To demonstrate the proper usage of the classifier() function that 
 #          is defined in classifier.py This function uses CNN model 
@@ -15,22 +15,36 @@
 # Usage: python test_classifier.py    -- will run program from commandline
 
 # Imports classifier function for using pretrained CNN to classify images 
-from classifier import classifier 
+from classifier import classifier
 
-# Defines a dog test image from pet_images folder
-test_image="pet_images/Collie_03797.jpg"
+# Test image
+test_image = "pet_images/Collie_03797.jpg"
 
-# Defines a model architecture to be used for classification
-# NOTE: this function only works for model architectures: 
-#      'vgg', 'alexnet', 'resnet'  
+# CNN architecture
 model = "vgg"
 
-# Demonstrates classifier() functions usage
-# NOTE: image_classication is a text string - It contains mixed case(both lower
-# and upper case letter) image labels that can be separated by commas when a 
-# label has more than one word that can describe it.
-image_classification = classifier(test_image, model)
+# Classify image
+image_classification = classifier(
+    test_image,
+    model
+)
 
-# prints result from running classifier() function
-print("\nResults from test_classifier.py\nImage:", test_image, "using model:",
-      model, "was classified as a:", image_classification)
+# Print result
+print(
+    "\nResults from test_classifier.py"
+)
+
+print(
+    "Image:",
+    test_image
+)
+
+print(
+    "Using model:",
+    model
+)
+
+print(
+    "Classified as:",
+    image_classification
+)
